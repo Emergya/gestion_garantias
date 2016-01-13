7f36e1d24616cf016406ae0d445f1dcfea582345
manuabalos <manu.abalos@gmail.com>
2016-01-13 18:01:29 +0100

    Añadido estilo del boton Eliminar del ANS

--- assets/javascripts/gg_articles.js
+++ assets/javascripts/gg_articles.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		$input_priority     = "<input class='t_ans' id='gg_article_gg_ans_attributes_"+i+"_priority' name='gg_article[gg_ans_attributes]["+i+"][priority]' size='30' type='text'>"
 		$label_description  = "<p class='l_ans'><label for='gg_article_gg_ans_attributes_"+i+"_Descripción: '>Descripción: </label></p>"
 		$input_description  = "<textarea class='t_ans' cols='40' id='gg_article_gg_ans_attributes_"+i+"_description' name='gg_article[gg_ans_attributes]["+i+"][description]' rows='7'></textarea>"
-		$link_delete        = "<a class='delete_ans' data-count="+ i +" data-confirm='¿Desea eliminar este artículo?'><span class='icon icon-del'>Eliminar</span></a>"
+		$link_delete        = "<a class='delete_ans' data-count="+ i +" data-confirm='¿Desea eliminar este artículo?'><span class='icon icon-del delete_text'>Eliminar</span></a>"
 		$div_fieldset_close = "</fieldset>"
 
 		$fieldset_complete = $div_fieldset_open + $label_priority + $input_priority + $label_description + $input_description + $link_delete + $div_fieldset_close
